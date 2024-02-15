a76d4d2592a70febf163724f1db6c363e052757a
MrSc33nsh0t <76527824+MrSc33nsh0t@users.noreply.github.com>
2024-02-15 18:11:42 +0100

    implémentation couleur

--- figures/couleur.cc
+++ figures/couleur.cc
@@ -1,1 +1,37 @@
 #include "couleur.hh"
+#include<iostream>
+#include <sstream>
+
+
+Couleur::Couleur(std::string couleur) {
+    if(couleur == "rouge") {
+        _couleur = "#FF0000";
+    }
+    else if (couleur == "bleu") {
+        _couleur = "#0000FF";
+    }
+    else if (couleur == "vert") {
+        _couleur = "#008000";
+    }
+    else if (couleur == "jaune") {
+        _couleur = "#FFFF00";
+    }
+    else if (couleur == "blanc") {
+        _couleur = "#FFFFFF";
+    }
+    else if (couleur == "noir") {
+        _couleur = "#000000";
+    }
+}
+    
+Couleur::Couleur(int r, int g, int b) {
+    _couleur = rgb2hex(r,g,b);
+}
+
+std::string Couleur::rgb2hex(int r, int g, int b) 
+{ 
+    std::stringstream ss;  
+    ss << "#"; 
+    ss << std::hex << (r << 16 | g << 8 | b ); 
+    return ss.str();
+}
--- figures/couleur.hh
+++ figures/couleur.hh
@@ -1,10 +1,15 @@
 #pragma once
+#include <string>
+
+// par soucis de simplicité, toutes les couleurs seront converties en hex
 class Couleur {
 public:
-    //TODO :
-    //couleur : rouge
-    //couleur : #hhhhhh
-    //couleur : rgb(int,int,int)
+    std::string _couleur;  // au format hex
+
     Couleur() = default;
+    Couleur(std::string couleur);  // au format texte, par exemple "rouge"
+    Couleur(int r, int g, int b);  // au format RGB
     ~Couleur() = default;
+
+    std::string rgb2hex(int r, int g, int b);
 };
