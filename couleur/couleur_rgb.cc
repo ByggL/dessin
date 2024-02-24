721deda66395a5ea215233064a34413cbd6463a5
rubenzhu <rubenzhu@hotmail.com>
2024-02-24 22:39:34 +0100

    rgb to hex fonction fixed.

--- couleur/couleur_rgb.cc
+++ couleur/couleur_rgb.cc
@@ -8,13 +8,27 @@ Couleur_rgb::Couleur_rgb(int r, int g, int b)
     _valeur = rgb2hex(r,g,b);
 }
 
-std::string Couleur_rgb::rgb2hex(int r, int g, int b)
-{
-    if((r > 255) | (g > 255) | (b > 255)){
+std::string Couleur_rgb::int_to_hex(int i ) {
+    std::stringstream stream;
+    if (i > 15) {
+        stream << std::hex << (i << 4) ;
+    } else {
+        stream << "0" + std::to_string(i);
+    }
+    return stream.str();
+}
+
+std::string Couleur_rgb::rgb2hex(int r, int g, int b) {
+    if((r > 255) | (g > 255) | (b > 255) |
+            (r < 0) | (g < 0) | (b < 0)){
         throw std::invalid_argument("Le RGB n'est pas correct");
     }
-    std::stringstream ss;
-    ss << "#";
-    ss << std::hex << (r << 16 | g << 8 | b );
-    return ss.str();
+
+    std::string s = "#";
+    std::string sr,sg,sb;
+    s += int_to_hex(r).substr(0,2);
+    s += int_to_hex(g).substr(0,2);
+    s += int_to_hex(b).substr(0,2);
+    return s;
 }
+
